46fa5cfb71e5d7965bd909d553c3c9dc62b8da89
Kent Hong <kent.n.hong@gmail.com>
2026-04-02 22:36:53 -0700

    Initial commit

---
+++ program_counter.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/02/2026 10:34:46 PM
+// Design Name: 
+// Module Name: program_counter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module program_counter(
+
+    );
+endmodule
