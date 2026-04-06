39498f42426db59e3fcb71249409ba8b82386442
Zey Ister <zeynettinisterca@gmail.com>
2026-04-05 22:10:54 -0700

    test commit

--- ALU.v
+++ ALU.v
@@ -1,11 +1,27 @@
-//Testy test test
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/05/2026 08:55:04 PM
+// Design Name: 
+// Module Name: adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
-module add_sub
-(
 
+module adder(
 
+    ); 
 
-
-
-
-)
+endmodule
